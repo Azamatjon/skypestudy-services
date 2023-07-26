b27a52baec9ff026732c61cbdad8d771182eb5ba
Azamatzhon <azamatjon.98@gmail.com>
2023-07-26 16:05:06 +0600

    upd

--- board-snapshot/src/app.module.ts
+++ board-snapshot/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {KafkaModule} from "./modules/common/modules/kafka/kafka.module";
+import {SnapshotModule} from "./modules/snapshot/snapshot.module";
 
 @Module({
   imports: [
@@ -8,7 +9,8 @@ import {KafkaModule} from "./modules/common/modules/kafka/kafka.module";
       isGlobal: true,
       envFilePath: [`${process.cwd()}/.env.${process.env.NODE_ENV}`, `${process.cwd()}/.env`]
     }),
-    KafkaModule
+    KafkaModule,
+    SnapshotModule
   ]
 })
 export class AppModule {}
